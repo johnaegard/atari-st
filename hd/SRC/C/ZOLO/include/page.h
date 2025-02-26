b5a7088ded8144348c841f1c915b33a037d276e5
John Aegard <johnzo@johnzo.com>
2025-02-25 22:32:34 -0800

    Page2 was missing memcopies

--- hd/SRC/C/ZOLO/include/page.h
+++ hd/SRC/C/ZOLO/include/page.h
@@ -11,23 +11,23 @@ typedef struct {
 } Page;
 
 typedef struct {
+  addr src;
+  addr dest;
+  word num_bytes;
+} MemcpyDef;
+
+typedef struct {
   Image image;
   void* base;
   word num_memcopies;
   word max_memcopies;
   word last_cx;
   word last_cy;
+  MemcpyDef memcopies[];
 } Page2;
-
 
 Page new_page(Image image);
 void swap_pages(Page* logical, Page* physical);
-
-typedef struct {
-  addr src;
-  addr dest;
-  word num_bytes;
-} MemcpyDef;
 
 Page2* new_page2(Image image, word max_memcopies);
 void swap_pages2(Page2** logical, Page2** physical);
